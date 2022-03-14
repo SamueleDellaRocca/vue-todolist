4dc19cd88d3db02ce1797a95d625868ef7e5ead2
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-14 15:32:31 +0100

    done== testo barrato

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const app = new Vue({
+
+    el: '#container',
+
+    data: {
+
+        arrayTodo: [
+
+            {
+                text: 'fare i compiti',
+                done: true,
+            },
+
+            {
+                text: 'sistemare la casa',
+                done: false,
+            },
+
+            {
+                text: 'scrivere codice',
+                done: true,
+            },
+
+            {
+                text: 'portare a spasso il cane',
+                done: true,
+            },
+
+            {
+                text: 'andare a fare la spesa',
+                done: false,
+            },
+
+            {
+                text: 'scrivere altro codice',
+                done: false,
+            },
+
+
+
+        ],
+
+
+    }
+
+
+
+
+});
